6489631505d8361a909601888fd30bbde60258b3
trozie <twanrozie@gmail.com>
2017-11-29 16:17:14 +0100

    making addrole test

--- src/test/java/com/capgemini/useradmin/controllers/RoleControllerTest.java
+++ src/test/java/com/capgemini/useradmin/controllers/RoleControllerTest.java
@@ -11,9 +11,11 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -25,19 +27,31 @@ public class RoleControllerTest {
     @Mock
     private RoleService roleService;
 
-    @Mock
-    private Role role;
-
     @InjectMocks
     private RoleController roleController;
 
     @Test
     public void getAllRoles() {
+        Role role = new Role();
+        role.setName("Pipo");
+        role.setUsers(Collections.emptyList());
+        role.setId(1);
         List<Role> roleList = new ArrayList<>();
         roleList.add(role);
         when(roleService.getAll()).thenReturn(roleList);
         Iterable<Role> roleIterable = roleController.getAll();
         verify(roleService, times(1)).getAll();
         assertEquals(1, roleIterable.spliterator().getExactSizeIfKnown());
+        Role ret = roleIterable.iterator().next();
+        assertTrue(ret.equals(role));
+    }
+
+    @Test
+    public void addRole() {
+        Role role = new Role();
+        role.setName("Pipo");
+        role.setUsers(Collections.emptyList());
+        role.setId(1);
+        verify(roleService, times(1)).add(role);
     }
 }
