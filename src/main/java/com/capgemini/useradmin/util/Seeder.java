461627c9b6a1c92c60cb12fc643adf5b9a3f57d0
Chaouki-Tiouassiouine <C.Tiouassiouine@Live.nl>
2017-11-29 15:36:16 +0100

    Perfectioned SeederDatabase

--- src/main/java/com/capgemini/useradmin/util/Seeder.java
+++ src/main/java/com/capgemini/useradmin/util/Seeder.java
@@ -8,14 +8,12 @@ import com.capgemini.useradmin.services.DefaultEntryService;
 import com.capgemini.useradmin.services.RoleService;
 import com.capgemini.useradmin.services.ScheduleEntryService;
 import com.capgemini.useradmin.services.UserService;
-import com.sun.xml.internal.ws.api.streaming.XMLStreamReaderFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
 
 public class Seeder {
 
@@ -28,117 +26,64 @@ public class Seeder {
     @Autowired
     private ScheduleEntryService scheduleEntryService;
 
-    public void seedUserTwan(){
-        User user = new User();
-        user.setFirstName("Twan");
-        user.setLastName("Rosie");
-        user.setStartDate(LocalDate.now().minusDays(1));
-        DefaultEntry defaultEntry = createDefaultEntry(user, DayOfWeek.THURSDAY, Shift.NIGHT);
-        ScheduleEntry scheduleEntry = createScheduleEntry(user, Shift.AFTERNOON, LocalDate.now());
-        Role role = createRole(user, "Manager");
-        addServices(user, role, scheduleEntry, defaultEntry);
+    public void seedUsers(){
+        createUser("Kok","Twan","Rozay", LocalDate.now().minusDays(2), DayOfWeek.MONDAY, Shift.AFTERNOON, Shift.AFTERNOON, LocalDate.now().minusDays(2));
+        createUser("Kok","Ramon","Vaan de", LocalDate.now().minusDays(10), DayOfWeek.SATURDAY, Shift.MORNING, Shift.AFTERNOON, LocalDate.now().minusDays(2));
+        createUser("Big Cock","Aydin","Erdas", LocalDate.now().minusDays(1), DayOfWeek.THURSDAY, Shift.NIGHT, Shift.MORNING, LocalDate.now().minusDays(2));
+        createUser("Kok","Emiel","Rossum van", LocalDate.now().minusDays(1), DayOfWeek.TUESDAY, Shift.AFTERNOON, Shift.EVENING, LocalDate.now().minusDays(2));
+        createUser("Kok","Frank","Noorloos", LocalDate.now().minusDays(6), DayOfWeek.WEDNESDAY, Shift.NIGHT, Shift.NIGHT, LocalDate.now().minusDays(2));
+        createUser("Kok","Jeffrey","Laarhoven van", LocalDate.now().minusDays(9), DayOfWeek.SUNDAY, Shift.AFTERNOON, Shift.MORNING, LocalDate.now().minusDays(2));
+        createUser("G","Chaouki","Tiouassiouine", LocalDate.now().minusDays(7), DayOfWeek.MONDAY, Shift.EVENING, Shift.NIGHT, LocalDate.now().minusDays(2));
+        createUser("Manager","Raymond","Lomman", LocalDate.now().minusDays(4), DayOfWeek.MONDAY, Shift.EVENING, Shift.MORNING, LocalDate.now().minusDays(2));
     }
 
-    public void seedUserRamon(){
+    public void createUser(String roleName, String firstName, String lastName, LocalDate startDate, DayOfWeek dayOfWeek, Shift shiftDefault, Shift shiftSchedule, LocalDate startSchedule){
         User user = new User();
-        user.setFirstName("Ramon");
-        user.setLastName("de Vaan");
-        user.setStartDate(LocalDate.now().minusDays(3));
-        DefaultEntry defaultEntry = createDefaultEntry(user, DayOfWeek.TUESDAY, Shift.EVENING);
-        ScheduleEntry scheduleEntry = createScheduleEntry(user, Shift.MORNING, LocalDate.now());
-        Role role = createRole(user, "Receptionist");
+        Role role = createRole(user, roleName);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setStartDate(startDate);
+        user.setScheduleEntries(new ArrayList<>());
+        user.setDefaultEntries(new ArrayList<>());
+        userService.add(user);
+        DefaultEntry defaultEntry = createDefaultEntry(user, dayOfWeek, shiftDefault);
+        ScheduleEntry scheduleEntry = createScheduleEntry(user, shiftSchedule, startSchedule);
         addServices(user, role, scheduleEntry, defaultEntry);
     }
-
-    public void seedUserEmiel(){
-        User user = new User();
-        user.setFirstName("Emiel");
-        user.setLastName("van Rossum");
-        user.setStartDate(LocalDate.now().minusDays(6));
-        DefaultEntry defaultEntry = createDefaultEntry(user, DayOfWeek.WEDNESDAY, Shift.MORNING);
-        ScheduleEntry scheduleEntry = createScheduleEntry(user, Shift.MORNING, LocalDate.now() );
-        Role role = createRole(user, "Kok");
-        addServices(user, role, scheduleEntry, defaultEntry);
-    }
-
-    public void seedUserAydin(){
-        User user = new User();
-        user.setFirstName("Aydin");
-        user.setLastName("Erdas");
-        user.setStartDate(LocalDate.now().minusDays(9));
-        DefaultEntry defaultEntry = createDefaultEntry(user, DayOfWeek.MONDAY, Shift.NIGHT);
-        ScheduleEntry scheduleEntry = createScheduleEntry(user, Shift.MORNING, LocalDate.now());
-        Role role = createRole(user, "HouseKeeper");
-        addServices(user, role, scheduleEntry, defaultEntry);
-    }
-
-    public void seedUserFrank(){
-        User user = new User();
-        user.setFirstName("Frank");
-        user.setLastName("Noorloos");
-        user.setStartDate(LocalDate.now().minusDays(3));
-        DefaultEntry defaultEntry = createDefaultEntry(user, DayOfWeek.THURSDAY, Shift.AFTERNOON);
-        ScheduleEntry scheduleEntry = createScheduleEntry(user, Shift.NIGHT, LocalDate.now());
-        Role role = createRole(user, "Manager");
-        addServices(user, role, scheduleEntry, defaultEntry);
-    }
-
-    public void seedUserJeffrey(){
-        User user = new User();
-        user.setFirstName("Jeffrey");
-        user.setLastName("van Laarhoven");
-        user.setStartDate(LocalDate.now().minusDays(2));
-        DefaultEntry defaultEntry = createDefaultEntry(user, DayOfWeek.SUNDAY, Shift.MORNING);
-        ScheduleEntry scheduleEntry = createScheduleEntry(user, Shift.EVENING, LocalDate.now());
-        Role role = createRole(user, "Receptionist");
-        addServices(user, role, scheduleEntry, defaultEntry);
-    }
-
-    public void seedUserChaouki(){
-        User user = new User();
-        user.setFirstName("Chaouki");
-        user.setLastName("Tiouassiouine");
-        user.setStartDate(LocalDate.now());
-        DefaultEntry defaultEntry = createDefaultEntry(user, DayOfWeek.TUESDAY, Shift.NIGHT);
-        ScheduleEntry scheduleEntry = createScheduleEntry(user, Shift.NIGHT, LocalDate.now());
-        Role role = createRole(user, "MasterChief");
-        addServices(user, role, scheduleEntry, defaultEntry);
-    }
-
 
     public Role createRole(User user, String roleName) {
         Role role = new Role();
-        List<User> userList = Arrays.asList(user);
-        role.setUsers(userList);
+        role.setUsers(new ArrayList<>());
         role.setName(roleName);
+        roleService.add(role);
+
         user.setRole(role);
+        role.setUsers(Arrays.asList(user));
         return role;
     }
 
     public DefaultEntry createDefaultEntry(User user, DayOfWeek day, Shift shift){
         DefaultEntry defaultEntry = new DefaultEntry();
-        List<DefaultEntry> defaultEntriesList = Arrays.asList(defaultEntry);
         defaultEntry.setUser(user);
         defaultEntry.setDay(day);
         defaultEntry.setShift(shift);
-        user.setDefaultEntries(defaultEntriesList);
+        user.setDefaultEntries(Arrays.asList(defaultEntry));
         return defaultEntry;
     }
 
     public ScheduleEntry createScheduleEntry(User user, Shift shift, LocalDate date){
         ScheduleEntry scheduleEntry = new ScheduleEntry();
-        List<ScheduleEntry> scheduleEntriesList = Arrays.asList(scheduleEntry);
         scheduleEntry.setUser(user);
         scheduleEntry.setDate(date);
         scheduleEntry.setShift(shift);
-        user.setScheduleEntries(scheduleEntriesList);
+        user.setScheduleEntries(Arrays.asList(scheduleEntry));
         return scheduleEntry;
     }
 
     public void addServices(User user, Role role, ScheduleEntry scheduleEntry, DefaultEntry defaultEntry) {
-        userService.add(user);
         defaultEntryService.add(defaultEntry);
-        roleService.add(role);
         scheduleEntryService.add(scheduleEntry);
+        userService.add(user);
+        roleService.add(role);
     }
 }
